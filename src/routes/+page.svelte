71f1618ae8f6959aa9cfa04b61e3bfef03bfbf1b
andrehebra <andrehebra@gmail.com>
2023-03-20 14:20:36 -0400

    added navbar with categories

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { Navbar, NavBrand, NavHamburger, NavUl, NavLi } from "flowbite-svelte";
+</script>
+
+
+<Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+      <img
+        src="https://flowbite.com/docs/images/logo.svg"
+        class="mr-3 h-6 sm:h-9"
+        alt="Flowbite Logo"
+      />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        Flowbite
+      </span>
+    </NavBrand>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden}>
+      <NavLi href="/" active={true}>Generating Inquiry</NavLi>
+      <NavLi href="/about">Multiple ways of writing</NavLi>
+      <NavLi href="/services">Information literacy</NavLi>
+      <NavLi href="/pricing">Research genre production</NavLi>
+      <NavLi href="/contact">Contributing knowledge</NavLi>
+      <NavLi href="/contact">Revision</NavLi>
+    </NavUl>
+  </Navbar>
